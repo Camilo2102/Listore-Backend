9960794b374bb5dd15cfcc7c2cb4660e6285f318
Camilo.muñoz <117365650+CamiloMD21@users.noreply.github.com>
2023-09-12 10:32:17 -0500

    Ajuste cors para frontend

--- src/main/java/com/example/listore/config/ListoreConfig.java
+++ src/main/java/com/example/listore/config/ListoreConfig.java
@@ -70,7 +70,7 @@ public class ListoreConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("https://listore-frontend.vercel.app")
                 .allowedMethods("PUT", "DELETE", "POST", "GET", "OPTIONS")
                 .allowedHeaders("Authorization", "Content-Type")
                 .allowCredentials(true)
